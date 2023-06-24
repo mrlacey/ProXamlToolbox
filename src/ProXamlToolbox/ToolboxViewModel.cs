2c58f31aea3d8c3f3580911edb9941964a93db39
Matt Lacey <matt@mrlacey.co.uk>
2023-06-24 16:46:37 +0100

    basic fake grouping support

--- src/ProXamlToolbox/ToolboxViewModel.cs
+++ src/ProXamlToolbox/ToolboxViewModel.cs
@@ -7,12 +7,44 @@ namespace ProXamlToolbox
     {
         public ToolboxViewModel()
         {
-            Items = GetDefaultItems();
+            LayoutItems = GetDefaultLayoutItems();
+            ControlItems = GetDefaultControlItems();
         }
 
-        public ObservableCollection<ProToolboxItem> Items { get; set; }
+        // TODO: Support proper grouped item lists
+        public ObservableCollection<ProToolboxItem> LayoutItems { get; set; }
+        public ObservableCollection<ProToolboxItem> ControlItems { get; set; }
 
-        private ObservableCollection<ProToolboxItem> GetDefaultItems()
+        private ObservableCollection<ProToolboxItem> GetDefaultLayoutItems()
+        {
+            return new ObservableCollection<ProToolboxItem> {
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Grid,
+                    DisplayedText = "Grid",
+                    DefaultContent = "<Grid [XN] RowDefinitions=\"Auto,Auto,*\" ColumnDefinitions=\"*,*\">\r\n*|*\r\n</Grid>",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Label,
+                    DisplayedText = "Label",
+                    DefaultContent = "<Label [XN] Text=\"CHANGEME\"[A11Y]SemanticProperties.HeadingLevel=\"Level1\" />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.TextBox,
+                    DisplayedText = "Entry",
+                    DefaultContent = "<Entry [XN] Text=\"{Binding PropertyName}\" Placeholder=\"CHANGEME\" />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Button,
+                    DisplayedText = "Button",
+                    DefaultContent = "<Button [XN][EVNT]Clicked=\"OnButtonClicked\"[CMD]Command=\"{Binding CommandName}\" Text=\"click me\"[A11Y]SemanticProperties.Hint=\"Add a description of what happens when clicked\" />\r\n",
+                },
+            };
+        }
+        private ObservableCollection<ProToolboxItem> GetDefaultControlItems()
         {
             return new ObservableCollection<ProToolboxItem> {
                 new ProToolboxItem

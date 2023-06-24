1a4cd73d0fdae4eda7056247717000ffe8feca2e
Matt Lacey <matt@mrlacey.co.uk>
2023-06-24 17:34:11 +0100

    add basic controls placeholders

--- src/ProXamlToolbox/ToolboxViewModel.cs
+++ src/ProXamlToolbox/ToolboxViewModel.cs
@@ -81,15 +81,63 @@ namespace ProXamlToolbox
             return new ObservableCollection<ProToolboxItem> {
                 new ProToolboxItem
                 {
-                    ImageMoniker = KnownMonikers.Grid,
-                    DisplayedText = "Grid",
-                    DefaultContent = "<Grid [XN] RowDefinitions=\"Auto,Auto,*\" ColumnDefinitions=\"*,*\">\r\n*|*\r\n</Grid>",
+                    ImageMoniker = KnownMonikers.Box,
+                    DisplayedText = "BoxView",
+                    DefaultContent = "<BoxView [XN] />\r\n",
                 },
                 new ProToolboxItem
                 {
-                    ImageMoniker = KnownMonikers.Label,
-                    DisplayedText = "Label",
-                    DefaultContent = "<Label [XN] Text=\"CHANGEME\"[A11Y]SemanticProperties.HeadingLevel=\"Level1\" />\r\n",
+                    ImageMoniker = KnownMonikers.Activity,
+                    DisplayedText = "ActivityIndicator",
+                    DefaultContent = "<ActivityIndicator [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.WebApplication,
+                    DisplayedText = "BlazorWebView",
+                    DefaultContent = "<BlazorWebView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.BorderElement,
+                    DisplayedText = "Border",
+                    DefaultContent = "<Border [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Button,
+                    DisplayedText = "Button",
+                    DefaultContent = "<Button [XN][EVNT]Clicked=\"OnButtonClicked\"[CMD]Command=\"{Binding CommandName}\" Text=\"click me\"[A11Y]SemanticProperties.Hint=\"Add a description of what happens when clicked\" />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Carousel,
+                    DisplayedText = "CarouselView",
+                    DefaultContent = "<CarouselView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.CheckBoxChecked,
+                    DisplayedText = "CheckBox",
+                    DefaultContent = "<CheckBox [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.ListView,
+                    DisplayedText = "CollectionView",
+                    DefaultContent = "<CollectionView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.DateTimePicker,
+                    DisplayedText = "DatePicker",
+                    DefaultContent = "<DatePicker [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Editor,
+                    DisplayedText = "Editor",
+                    DefaultContent = "<Editor [XN] />\r\n",
                 },
                 new ProToolboxItem
                 {
@@ -99,9 +147,111 @@ namespace ProXamlToolbox
                 },
                 new ProToolboxItem
                 {
-                    ImageMoniker = KnownMonikers.Button,
-                    DisplayedText = "Button",
-                    DefaultContent = "<Button [XN][EVNT]Clicked=\"OnButtonClicked\"[CMD]Command=\"{Binding CommandName}\" Text=\"click me\"[A11Y]SemanticProperties.Hint=\"Add a description of what happens when clicked\" />\r\n",
+                    //ImageMoniker = KnownMonikers.ListView,
+                    DisplayedText = "GraphicsView",
+                    DefaultContent = "<GraphicsView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Image,
+                    DisplayedText = "Image",
+                    DefaultContent = "<Image [XN] Source=\"PATH-TO-IMAGE\" />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.ImageButton,
+                    DisplayedText = "ImageButton",
+                    DefaultContent = "<ImageButton [XN] Source=\"PATH-TO-IMAGE\" />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Label,
+                    DisplayedText = "Label",
+                    DefaultContent = "<Label [XN] Text=\"CHANGEME\"[A11Y]SemanticProperties.HeadingLevel=\"Level1\" />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.ListView,
+                    DisplayedText = "ListView",
+                    DefaultContent = "<ListView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.MapTileLayer,
+                    DisplayedText = "Map",
+                    DefaultContent = "<!-- Make sure you add the NuGet package Microsoft.Maui.Controls.Maps -->\r\n<map:Map [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Picker,
+                    DisplayedText = "Picker",
+                    DefaultContent = "<Picker [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.ProgressBar,
+                    DisplayedText = "ProgressBar",
+                    DefaultContent = "<ProgressBar [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.RadioButton,
+                    DisplayedText = "RadioButton",
+                    DefaultContent = "<RadioButton [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Refresh,
+                    DisplayedText = "RefreshView",
+                    DefaultContent = "<RefreshView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Search,
+                    DisplayedText = "SearchBar",
+                    DefaultContent = "<SearchBar [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.Slider,
+                    DisplayedText = "Slider",
+                    DefaultContent = "<Slider [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.ButtonGroup,
+                    DisplayedText = "Stepper",
+                    DefaultContent = "<Stepper [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    //ImageMoniker = KnownMonikers.ToggleButton,
+                    DisplayedText = "SwipView",
+                    DefaultContent = "<SwipView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.ToggleButton,
+                    DisplayedText = "Switch",
+                    DefaultContent = "<Switch [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.AppearanceGrid,
+                    DisplayedText = "TableView",
+                    DefaultContent = "<TableView [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.TimePicker,
+                    DisplayedText = "TimePicker",
+                    DefaultContent = "<TimePicker [XN] />\r\n",
+                },
+                new ProToolboxItem
+                {
+                    ImageMoniker = KnownMonikers.WebApplication,
+                    DisplayedText = "WebView",
+                    DefaultContent = "<WebView [XN] />\r\n",
                 },
             };
         }

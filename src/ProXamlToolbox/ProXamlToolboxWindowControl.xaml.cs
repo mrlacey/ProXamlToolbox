4746871e5b1b0fa78cbf59adba4236d3a6ef85bc
Matt Lacey <matt@mrlacey.co.uk>
2023-06-24 11:18:14 +0100

    Initial project

---
+++ src/ProXamlToolbox/ProXamlToolboxWindowControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ProXamlToolbox
+{
+    public partial class ProXamlToolboxWindowControl : UserControl
+    {
+        public ProXamlToolboxWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "ProXamlToolboxWindow");
+        }
+    }
+}

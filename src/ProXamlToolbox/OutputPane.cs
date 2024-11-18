17f663d5bd6c73b84aab809f1006964f6fa51d2c
Matt Lacey <matt@mrlacey.co.uk>
2024-11-18 09:09:32 +0000

    Add basic analytics tracking

---
+++ src/ProXamlToolbox/OutputPane.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace ProXamlToolbox
+{
+	public class OutputPane
+	{
+		private static Guid pxtPaneGuid = new Guid("002A8552-5B63-4765-9970-69B016BB9AF4");
+
+		private static OutputPane instance;
+
+		private readonly IVsOutputWindowPane pane;
+
+		private OutputPane()
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			if (ServiceProvider.GlobalProvider.GetService(typeof(SVsOutputWindow)) is IVsOutputWindow outWindow
+			 && (ErrorHandler.Failed(outWindow.GetPane(ref pxtPaneGuid, out pane)) || pane == null))
+			{
+				if (ErrorHandler.Failed(outWindow.CreatePane(ref pxtPaneGuid, Vsix.Name, 1, 0)))
+				{
+					System.Diagnostics.Debug.WriteLine("Failed to create the Output window pane.");
+					return;
+				}
+
+				if (ErrorHandler.Failed(outWindow.GetPane(ref pxtPaneGuid, out pane)) || (pane == null))
+				{
+					System.Diagnostics.Debug.WriteLine("Failed to get access to the Output window pane.");
+				}
+			}
+		}
+
+		public static OutputPane Instance => instance ??= new OutputPane();
+
+		public async Task ActivateAsync()
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+			pane?.Activate();
+		}
+
+		public async Task WriteAsync(string message)
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+			_ = (pane?.OutputStringThreadSafe($"{message}{Environment.NewLine}"));
+		}
+	}
+
+}
--- src/ProXamlToolbox/ProXamlToolboxWindowPackage.cs
+++ src/ProXamlToolbox/ProXamlToolboxWindowPackage.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Community.VisualStudio.Toolkit;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
@@ -45,6 +48,41 @@ namespace ProXamlToolbox
 
 			await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 			await this.RegisterCommandsAsync();
+
+			await TrackBasicUsageAnalyticsAsync();
+		}
+
+		private static async Task TrackBasicUsageAnalyticsAsync()
+		{
+			try
+			{
+#if !DEBUG
+			if (string.IsNullOrWhiteSpace(AnalyticsConfig.TelemetryConnectionString))
+			{
+				return;
+			}
+
+			var config = new TelemetryConfiguration
+			{
+				ConnectionString = AnalyticsConfig.TelemetryConnectionString,
+			};
+
+			var client = new TelemetryClient(config);
+
+			var properties = new Dictionary<string, string>
+				{
+					{ "VsixVersion", Vsix.Version },
+					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+				};
+
+			client.TrackEvent(Vsix.Name, properties);
+#endif
+			}
+			catch (Exception exc)
+			{
+				System.Diagnostics.Debug.WriteLine(exc);
+				await OutputPane.Instance.WriteAsync("Error tracking usage analytics: " + exc.Message);
+			}
 		}
 	}
 }

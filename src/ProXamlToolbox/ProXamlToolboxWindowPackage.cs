1b3c090f9430979c95e32787d5c2584290da1f7d
Matt Lacey <matt@mrlacey.co.uk>
2024-12-09 09:19:39 +0000

    update analytics data

--- src/ProXamlToolbox/ProXamlToolboxWindowPackage.cs
+++ src/ProXamlToolbox/ProXamlToolboxWindowPackage.cs
@@ -73,6 +73,8 @@ namespace ProXamlToolbox
 				{
 					{ "VsixVersion", Vsix.Version },
 					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+					{ "Architecture", RuntimeInformation.ProcessArchitecture.ToString() },
+					{ "MsInternal", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.IsUserMicrosoftInternal.ToString() },
 				};
 
 			client.TrackEvent(Vsix.Name, properties);
